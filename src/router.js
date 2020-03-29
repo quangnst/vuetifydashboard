b6e03a26e5add3e1d8f6fd7d86b3dc25735461b7
Quang <quangnst@gmail.com>
2020-03-29 20:58:28 +0700

    update

--- src/router.js
+++ src/router.js
@@ -23,32 +23,11 @@ export default new Router({
           path: 'pages/user',
           component: () => import('@/views/dashboard/pages/UserProfile'),
         },
-        {
-          name: 'Notifications',
-          path: 'components/notifications',
-          component: () => import('@/views/dashboard/component/Notifications'),
-        },
-        {
-          name: 'Icons',
-          path: 'components/icons',
-          component: () => import('@/views/dashboard/component/Icons'),
-        },
-        {
-          name: 'Typography',
-          path: 'components/typography',
-          component: () => import('@/views/dashboard/component/Typography'),
-        },
         // Tables
         {
           name: 'Regular Tables',
           path: 'tables/regular-tables',
           component: () => import('@/views/dashboard/tables/RegularTables'),
-        },
-        // Maps
-        {
-          name: 'Google Maps',
-          path: 'maps/google-maps',
-          component: () => import('@/views/dashboard/maps/GoogleMaps'),
         },
         // Upgrade
         {
--- src/views/dashboard/components/core/Drawer.vue
+++ src/views/dashboard/components/core/Drawer.vue
@@ -121,26 +121,6 @@
           icon: 'mdi-clipboard-outline',
           to: '/tables/regular-tables',
         },
-        {
-          title: 'typography',
-          icon: 'mdi-format-font',
-          to: '/components/typography',
-        },
-        {
-          title: 'icons',
-          icon: 'mdi-chart-bubble',
-          to: '/components/icons',
-        },
-        {
-          title: 'google',
-          icon: 'mdi-map-marker',
-          to: '/maps/google-maps',
-        },
-        {
-          title: 'notifications',
-          icon: 'mdi-bell',
-          to: '/components/notifications',
-        },
       ],
     }),
 
